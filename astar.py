7ec7c3fe539b162ef5cf41798493f417995a8442
j-o-s-e-p-h-h <josephmuleipropilot@gmail.com>
2026-03-28 17:25:18 +0200

    Update astar.py

---
+++ astar.py
@@ -0,0 +1,7 @@
+import pygame 
+import math 
+from queue import PriorityQueue 
+
+width = 800
+window = pygame.display.set_mode((width, width))
+pygame.display.set_caption("A* Star Path Finding Algorithim")
